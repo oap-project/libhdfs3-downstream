0335f9011c0c4719093c58ebca8f40b6103dc296
Yuan Zhou <yuan.zhou@intel.com>
2021-03-16 10:53:26 +0800

    adding default value for domain socket path
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- libhdfs3/src/common/SessionConfig.cpp
+++ libhdfs3/src/common/SessionConfig.cpp
@@ -165,7 +165,7 @@ SessionConfig::SessionConfig(const Config & conf) {
         {&rpcAuthMethod, "hadoop.security.authentication", "simple" },
         {&kerberosCachePath, "hadoop.security.kerberos.ticket.cache.path", "" },
         {&logSeverity, "dfs.client.log.severity", "INFO" },
-        {&domainSocketPath, "dfs.domain.socket.path", ""},
+        {&domainSocketPath, "dfs.domain.socket.path", "/var/lib/hadoop-hdfs/dn_socket"},
         {&kmsUrl, "dfs.encryption.key.provider.uri", "" },
         {&rpcProtectionStr, "hadoop.rpc.protection", ""},
         {&dataProtectionStr, "dfs.data.transfer.protection", ""},
